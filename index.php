9e45afdaa6e42f09417ca06eada488ae8ab9a87d
fndomariano <fernando.mar16@gmail.com>
2014-04-16 19:36:05 -0300

    Corrigido o problema na aba e adicionada grid

--- index.php
+++ index.php
@@ -9,18 +9,24 @@
  
  	<header></header>
  	
-	<nav>
-		<ul class="nav nav-pills">
-			<li><a href="#grupoA">Grupo A</a></li>
-			<li><a href="#grupoB">Grupo B</a></li>
-			<li><a href="#grupoC">Grupo C</a></li>
-			<li><a href="#grupoD">Grupo D</a></li>
-			<li><a href="#grupoE">Grupo E</a></li>
-			<li><a href="#grupoF">Grupo F</a></li>
-			<li><a href="#grupoG">Grupo G</a></li>
-			<li><a href="#grupoH">Grupo H</a></li>    
-		</ul>
-	</nav>
+ 	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<nav>
+					<ul class="nav nav-pills">
+						<li class="active"><a href="#grupoA">Grupo A</a></li>
+						<li><a href="#grupoB">Grupo B</a></li>
+						<li><a href="#grupoC">Grupo C</a></li>
+						<li><a href="#grupoD">Grupo D</a></li>
+						<li><a href="#grupoE">Grupo E</a></li>
+						<li><a href="#grupoF">Grupo F</a></li>
+						<li><a href="#grupoG">Grupo G</a></li>
+						<li><a href="#grupoH">Grupo H</a></li>    
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</div>
 	
 	<section class="container">
 		<div class="row">
@@ -83,72 +89,73 @@
 			  		</tbody>
 				</table>
 			</div>
+		
+			<div class="col-md-12" id="grupoB">
+				<table class="table table-hover table-bordered">
+			    	<caption>Grupo B</caption>
+			    	<thead>
+				    	<tr>
+					        <th>Equipe</th>
+					        <th>J</th>
+					        <th>V</th>
+					        <th>E</th>
+					        <th>D</th>
+					        <th>GF</th>
+					        <th>GS</th>
+					        <th>Pts</th>
+				    	</tr>
+				    </thead>
+				    <tbody>
+			      		<tr>
+							<td>Brazil</td>
+							<td>0</td>
+							<td>0</td>
+							<td>0</td>
+							<td>0</td>
+							<td>0</td>
+							<td>0</td>
+							<td>0</td>
+						</tr>
+						<tr>
+							<td>Croácia</td>
+							<td>0</td>
+							<td>0</td>
+							<td>0</td>
+							<td>0</td>
+							<td>0</td>
+							<td>0</td>
+							<td>0</td>
+						</tr>
+						<tr>
+							<td>México</td>
+							<td>0</td>
+							<td>0</td>
+							<td>0</td>
+							<td>0</td>
+							<td>0</td>
+							<td>0</td>
+							<td>0</td>
+						</tr>
+						<tr>
+							<td>Camarões</td>
+							<td>0</td>
+							<td>0</td>
+							<td>0</td>
+							<td>0</td>
+							<td>0</td>
+							<td>0</td>
+							<td>0</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<div class="col-md-12" id="grupoC"></div>
+			<div class="col-md-12" id="grupoD"></div>
+			<div class="col-md-12" id="grupoE"></div>
+			<div class="col-md-12" id="grupoF"></div>
+			<div class="col-md-12" id="grupoG"></div>
+			<div class="col-md-12" id="grupoH"></div>
 		</div>
-		<div class="col-md-12" id="grupoB">
-			<table class="table table-hover">
-		    	<caption>Grupo B</caption>
-		    	<thead>
-			    	<tr>
-				        <th>Time</th>
-				        <th>J</th>
-				        <th>V</th>
-				        <th>E</th>
-				        <th>D</th>
-				        <th>GF</th>
-				        <th>GS</th>
-				        <th>Pts</th>
-			    	</tr>
-			    </thead>
-			    <tbody>
-		      		<tr>
-						<td>Brazil</td>
-						<td>0</td>
-						<td>0</td>
-						<td>0</td>
-						<td>0</td>
-						<td>0</td>
-						<td>0</td>
-						<td>0</td>
-					</tr>
-					<tr>
-						<td>Croácia</td>
-						<td>0</td>
-						<td>0</td>
-						<td>0</td>
-						<td>0</td>
-						<td>0</td>
-						<td>0</td>
-						<td>0</td>
-					</tr>
-					<tr>
-						<td>México</td>
-						<td>0</td>
-						<td>0</td>
-						<td>0</td>
-						<td>0</td>
-						<td>0</td>
-						<td>0</td>
-						<td>0</td>
-					</tr>
-					<tr>
-						<td>Camarões</td>
-						<td>0</td>
-						<td>0</td>
-						<td>0</td>
-						<td>0</td>
-						<td>0</td>
-						<td>0</td>
-						<td>0</td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
-		<div class="col-md-12" id="grupoC"></div>
-		<div class="col-md-12" id="grupoD"></div>
-		<div class="col-md-12" id="grupoE"></div>
-		<div class="col-md-12" id="grupoF"></div>
-		<div class="col-md-12" id="grupoG"></div>
-		<div class="col-md-12" id="grupoH"></div>
 	</section>
 
 	<script src="js/jquery-1.11.0.min.js"></script>
--- js/world-cup.js
+++ js/world-cup.js
@@ -4,24 +4,16 @@ $(document).ready(function(){
 	
 	$("nav ul li a").on('click', function(){
 		
-		// var valorHref = $(this).attr('href');
-
 		var divId = $('li.active').children().attr('href');
+		
 		$(divId).css('display', 'none');
 		
-		console.log(divId);
-
 		$(this).parent().siblings('li').removeClass('active');
 		$(this).parent().addClass('active');
-		var divId = $('li.active').children().attr('href');
+		
+		divId = $('li.active').children().attr('href');
 
 		$(divId).css('display', 'block');
-		
-		console.log(divId);	
-		// if($(this).parent().prop('class', 'active')){
-		// 	$(this).prop('class', '');
-		// 	$(valorHref).show();	
-		// }
 		
 		
 	});
