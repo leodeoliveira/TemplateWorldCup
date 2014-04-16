ce02d4179154192cf0eaa3bde1b2cc98a5ca4188
fndomariano <fernando.mar16@gmail.com>
2014-04-16 15:28:55 -0300

    Feita a parte de abas

--- index.php
+++ index.php
@@ -1,135 +1,159 @@
 <!DOCTYPE HTML>
 <html>
-	<head>
-		<meta charset="UTF-8"> 
-		<script>window.jQuery || document.write('<script src="js/jquery-1.7.1.min.js"><\/script>')</script> 
-		<link href="css/bootstrap.css" rel="stylesheet" type="text/css" /> 
-		<script src="js/bootstrap.min.js"></script>
-		<title>Fifa World Cup - 2014</title>
-	</head>
-<body style="width:'80%'">
-  <header></header>
-  <nav>
-  <ul class="nav nav-pills">
-    <li class="active"><a href="#">Grupo A</a></li>
-    <li><a href="#" data-ref="#grupoB" model-ref="#grupoB">Grupo B</a></li>
-    <li><a href="#">Grupo C</a></li>
-    <li><a href="#">Grupo D</a></li>
-    <li><a href="#">Grupo E</a></li>
-    <li><a href="#">Grupo F</a></li>
-    <li><a href="#">Grupo G</a></li>
-    <li><a href="#">Grupo H</a></li>    
-  </ul>
-  </nav>
-    <table class="table table-hover">
-      <caption>Grupo A</caption>
-    	<tr>
-    		<th>Equipe</th>
-    		<th>J</th>
-    		<th>V</th>
-    		<th>E</th>
-    		<th>D</th>
-    		<th>GF</th>
-    		<th>GS</th>
-    		<th>Pts</th>
+<head>
+	<title>Fifa World Cup - 2014</title>
+	<meta charset="UTF-8"> 
+	<link href="css/bootstrap.css" rel="stylesheet" type="text/css" /> 
+</head>
+<body>
+ 
+ 	<header></header>
+ 	
+	<nav>
+		<ul class="nav nav-pills">
+			<li><a href="#grupoA">Grupo A</a></li>
+			<li><a href="#grupoB">Grupo B</a></li>
+			<li><a href="#grupoC">Grupo C</a></li>
+			<li><a href="#grupoD">Grupo D</a></li>
+			<li><a href="#grupoE">Grupo E</a></li>
+			<li><a href="#grupoF">Grupo F</a></li>
+			<li><a href="#grupoG">Grupo G</a></li>
+			<li><a href="#grupoH">Grupo H</a></li>    
+		</ul>
+	</nav>
+	
+	<section class="container">
+		<div class="row">
+		    <div class="col-md-12" id="grupoA">
+			    <table class="table table-hover table-bordered">
+			    	<caption>Grupo A</caption>
+			    	<thead>
+				    	<tr>
+				    		<th>Equipe</th>
+				    		<th>J</th>
+				    		<th>V</th>
+				    		<th>E</th>
+				    		<th>D</th>
+				    		<th>GF</th>
+				    		<th>GS</th>
+				    		<th>Pts</th>
+				    	</tr>
+			    	</thead>
+			  		<tbody>
+					  	<tr>
+					  		<td>Brazil</td>
+					  		<td>0</td>
+					  		<td>0</td>
+					  		<td>0</td>
+					  		<td>0</td>
+					  		<td>0</td>
+					  		<td>0</td>
+					  		<td>0</td>
+					  	</tr>
+					  	<tr>
+					  		<td>Croácia</td>
+					  		<td>0</td>
+					  		<td>0</td>
+					  		<td>0</td>
+					  		<td>0</td>
+					  		<td>0</td>
+					  		<td>0</td>
+					  		<td>0</td>
+					  	</tr>
+					  	<tr>
+					  		<td>México</td>
+					  		<td>0</td>
+					  		<td>0</td>
+					  		<td>0</td>
+					  		<td>0</td>
+					  		<td>0</td>
+					  		<td>0</td>
+					  		<td>0</td>
+					  	</tr>
+					  	<tr>
+					  		<td>Camarões</td>
+					  		<td>0</td>
+					  		<td>0</td>
+					  		<td>0</td>
+					  		<td>0</td>
+					  		<td>0</td>
+					  		<td>0</td>
+					  		<td>0</td>
+					  	</tr>
+			  		</tbody>
+				</table>
+			</div>
+		</div>
+		<div class="col-md-12" id="grupoB">
+			<table class="table table-hover">
+		    	<caption>Grupo B</caption>
+		    	<thead>
+			    	<tr>
+				        <th>Time</th>
+				        <th>J</th>
+				        <th>V</th>
+				        <th>E</th>
+				        <th>D</th>
+				        <th>GF</th>
+				        <th>GS</th>
+				        <th>Pts</th>
+			    	</tr>
+			    </thead>
+			    <tbody>
+		      		<tr>
+						<td>Brazil</td>
+						<td>0</td>
+						<td>0</td>
+						<td>0</td>
+						<td>0</td>
+						<td>0</td>
+						<td>0</td>
+						<td>0</td>
+					</tr>
+					<tr>
+						<td>Croácia</td>
+						<td>0</td>
+						<td>0</td>
+						<td>0</td>
+						<td>0</td>
+						<td>0</td>
+						<td>0</td>
+						<td>0</td>
+					</tr>
+					<tr>
+						<td>México</td>
+						<td>0</td>
+						<td>0</td>
+						<td>0</td>
+						<td>0</td>
+						<td>0</td>
+						<td>0</td>
+						<td>0</td>
+					</tr>
+					<tr>
+						<td>Camarões</td>
+						<td>0</td>
+						<td>0</td>
+						<td>0</td>
+						<td>0</td>
+						<td>0</td>
+						<td>0</td>
+						<td>0</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		<div class="col-md-12" id="grupoC"></div>
+		<div class="col-md-12" id="grupoD"></div>
+		<div class="col-md-12" id="grupoE"></div>
+		<div class="col-md-12" id="grupoF"></div>
+		<div class="col-md-12" id="grupoG"></div>
+		<div class="col-md-12" id="grupoH"></div>
+	</section>
 
-    	</tr>
-  	<tr>
-  		<td>Brazil</td>
-  		<td>0</td>
-  		<td>0</td>
-  		<td>0</td>
-  		<td>0</td>
-  		<td>0</td>
-  		<td>0</td>
-  		<td>0</td>
-  	</tr>
-  	<tr>
-  		<td>Croácia</td>
-  		<td>0</td>
-  		<td>0</td>
-  		<td>0</td>
-  		<td>0</td>
-  		<td>0</td>
-  		<td>0</td>
-  		<td>0</td>
-  	</tr>
-  	<tr>
-  		<td>México</td>
-  		<td>0</td>
-  		<td>0</td>
-  		<td>0</td>
-  		<td>0</td>
-  		<td>0</td>
-  		<td>0</td>
-  		<td>0</td>
-  	</tr>
-  	<tr>
-  		<td>Camarões</td>
-  		<td>0</td>
-  		<td>0</td>
-  		<td>0</td>
-  		<td>0</td>
-  		<td>0</td>
-  		<td>0</td>
-  		<td>0</td>
-  	</tr>
-	</table>
-  <table class="table table-hover" id="grupoB">
-      <caption>Grupo B</caption>
-      <tr>
-        <th>Time</th>
-        <th>J</th>
-        <th>V</th>
-        <th>E</th>
-        <th>D</th>
-        <th>GF</th>
-        <th>GS</th>
-        <th>Pts</th>
-
-      </tr>
-    <tr>
-      <td>Brazil</td>
-      <td>0</td>
-      <td>0</td>
-      <td>0</td>
-      <td>0</td>
-      <td>0</td>
-      <td>0</td>
-      <td>0</td>
-    </tr>
-    <tr>
-      <td>Croácia</td>
-      <td>0</td>
-      <td>0</td>
-      <td>0</td>
-      <td>0</td>
-      <td>0</td>
-      <td>0</td>
-      <td>0</td>
-    </tr>
-    <tr>
-      <td>México</td>
-      <td>0</td>
-      <td>0</td>
-      <td>0</td>
-      <td>0</td>
-      <td>0</td>
-      <td>0</td>
-      <td>0</td>
-    </tr>
-    <tr>
-      <td>Camarões</td>
-      <td>0</td>
-      <td>0</td>
-      <td>0</td>
-      <td>0</td>
-      <td>0</td>
-      <td>0</td>
-      <td>0</td>
-    </tr>
-  </table>
-</form>
+	<script src="js/jquery-1.11.0.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/world-cup.js"></script>
 </body>
 
 </html>
